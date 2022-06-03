a80831fd8afba392170dd40fa2ab462f9afb1793
Tair Yeruham <76681385+Tair-Yeruham@users.noreply.github.com>
2022-06-03 13:20:50 +0300

    Add files via upload

---
+++ Proxy/PostProxy.cs
@@ -0,0 +1,24 @@
+﻿using FacebookWrapper.ObjectModel;
+
+namespace B22_Ex03.Proxy
+{
+    public class PostProxy
+    {
+        public Post Post { get; set; }
+
+        public PostProxy(Post i_Post)
+        {
+            Post = i_Post;
+        }
+
+        public bool CheckIfMessageIsNotNull()
+        {
+            return Post.Message != null;
+        }
+
+        public bool CheckIfPostMessageContainStringFromUser(string i_SearchPost)
+        {
+            return Post.Message.ToLower().Contains(i_SearchPost.ToLower());
+        }
+    }
+}
---
+++ Proxy/UserProxy.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using B22_Ex03.Enums;
+using B22_Ex03.Models;
+using B22_Ex03.Services;
+using FacebookWrapper.ObjectModel;
+
+namespace B22_Ex03.Proxy
+{
+    public class UserProxy
+    {
+        private static readonly string sr_Path = Path.Combine(PathHelperService.sr_PathToFiles + @"Administrator.xml");
+        private readonly User r_User;
+
+        public UserProxy(User i_User)
+        {
+            r_User = i_User;
+            UserType = setUserType();
+        }
+
+        public eUserType UserType { get; set; }
+
+        private eUserType setUserType()
+        {
+            Administrator administratorUsers = XmlHelperService.LoadXmlData<Administrator>(sr_Path);
+            
+            return administratorUsers.Name.Contains(r_User.Name) ? eUserType.Administrator : eUserType.NotAdministrator;
+        }
+    }
+}
