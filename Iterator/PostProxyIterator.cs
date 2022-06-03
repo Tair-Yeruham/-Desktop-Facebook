03694039a554dda1becc37f476fa2e925577e07e
Tair Yeruham <76681385+Tair-Yeruham@users.noreply.github.com>
2022-06-03 13:15:58 +0300

    Add files via upload

---
+++ Iterator/PostProxyIterator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using B22_Ex03.Proxy;
+using FacebookWrapper.ObjectModel;
+
+namespace B22_Ex03.Iterator
+{
+    public static class PostProxyIterator
+    {
+        public static IEnumerable<PostProxy> GetPostProxyIterator(IEnumerable<Post> i_Posts)
+        {
+            foreach(Post post in i_Posts)
+            {
+                PostProxy postProxy = new PostProxy(post);
+
+                if(postProxy.CheckIfMessageIsNotNull())
+                {
+                    yield return postProxy;
+                }
+            }
+        }
+    }
+}
